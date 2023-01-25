d1bdabb04f4677e1cc3c28da98eeeadb12d6c2b9
Arturo Cunillas Valadez <Arturo.valadez1@aol.com>
2023-01-24 17:46:24 -0800

    updatedreadmefile

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # LAB - 2
 
+## About-me
 
-## about-me
+The about-me page contains info about myself, my goals, my life, and contains some interaction with the client through javaScript.
 
-### Author: 
+### Author: Arturo C Valadez/Arturo, Kam, Kirk, Deon.
+
+### Links and Resources
+
+* [JS help](https://www.w3schools.com/js/js_variables.asp)
+* [201 class repo](https://github.com/codefellows/seattle-code-201d95/tree/main/class-02)
+
+### Lighthouse Accessibility Report Score
+
+[](img/Lighthouseclass2.png)
+
+### Reflections and Comments
+
+* It was difficult trying to keep track of my code and all its changes.
+* I learned how to make a background image in css, also learned some good coding practices to keep everything organized.
--- js/app.js
+++ js/app.js
@@ -1,47 +1,47 @@
 'use strict';
 alert ('Hey there!');
-console.log ('hello');
+// console.log ('hello');
 
 let username = prompt ('What is your name?');
-console.log('What is your name?');
+// console.log('What is your name?');
 alert ('Hello ' + username );
 
 let home = prompt('Have I lived in another country?').toLowerCase();
 
 if (home === 'yes' || home === 'y'){
-  console.log ('You are correct ' + username);
+  // console.log ('You are correct ' + username);
   alert('You are correct ' + username);
 } else if (home === 'no' || home === 'n'){
-  console.log ('Almost, try again!');
+  // console.log ('Almost, try again!');
   alert('Almost, try again! ');
 } else {
-  console.log ('Oops, thats not it');
+  // console.log ('Oops, thats not it');
   alert('Oops, thats not it.');
 }
 
 let Navy = prompt ('Was I in the U.S. Army?').toLowerCase();
 
 if (Navy === 'yes' || Navy === 'y'){
-  console.log('Wrong answer, you can find out more on this page.');
+  // console.log('Wrong answer, you can find out more on this page.');
   alert('Wrong answer, you can find out more on this page.');
 } else if (Navy === 'no' || Navy === 'n'){
-  console.log ('You are right! I was in the U.S. Navy');
+  // console.log ('You are right! I was in the U.S. Navy');
   alert('You are right! I was in the U.S. Navy.');
 } else {
-  console.log ('Try again.');
+  // console.log ('Try again.');
   alert('Oops, thats not it.');
 }
 
 let interests = prompt ('Am I a front end developer?').toLowerCase();
 
 if (interests === 'yes' || interests === 'y'){
-  console.log ('You are right! Nice.');
+  // console.log ('You are right! Nice.');
   alert('You are right! Nice.');
 } else if (interests === 'no' || interests === 'n'){
-  console.log('Actually, I am a front end developer.');
+  // console.log('Actually, I am a front end developer.');
   alert('Actually, I am  a front end developer.');
 } else {
-  console.log ('Oops, thats not it.');
+  // console.log ('Oops, thats not it.');
   alert('Oops, thats not it.');
 }
 
@@ -49,13 +49,13 @@ let hobbies = prompt ('Do I like music?')
   .toLowerCase();
 
 if (hobbies === 'yes' || hobbies === 'y'){
-  console.log ('You are right, I do.');
+  // console.log ('You are right, I do.');
   alert('You are right, I do.');
 } else if ( hobbies === 'no' || hobbies === 'n'){
-  console.log ('I love music!');
+  // console.log ('I love music!');
   alert('I love music!');
 } else {
-  console.log ('Try again.');
+  // console.log ('Try again.');
   alert('Oops, thats not it.');
 }
 
@@ -63,14 +63,14 @@ let pet = prompt ('Do I own a dog?')
   .toLowerCase();
 
 if (pet === 'yes' || pet === 'y'){
-  console.log ('you are right, I do.');
+  // console.log ('you are right, I do.');
   alert('you are right, I do.');
 } else if ( pet === 'no' || pet === 'no') {
-  console.log (' I actually do, I own a malinois mix.');
+  // console.log (' I actually do, I own a malinois mix.');
   alert('I actually do, I own a malinois mix.');
 } else {
-  console.log ('oops, thats not it.');
+  // console.log ('oops, thats not it.');
   alert('Oops, thats not it.');
 }
 
-alert('Welcome to my page ' + username);
+alert('Welcome to my page. Thank you for taking this quiz ' + username);
